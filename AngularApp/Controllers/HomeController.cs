b4996130a0be6b61beef53cb4b8e07624083b500
Filip Majdzik <filip@majdzik.com>
2015-12-04 07:51:45 +0100

    + gen for angular rout

--- AngularApp/Controllers/HomeController.cs
+++ AngularApp/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace AngularApp.Controllers
 {
+    public class Route
+    {
+        public string Controller;
+        public string Action;
+        public string Attributes;
+        public string ReturnType;
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -25,6 +35,21 @@ namespace AngularApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Route()
+        {
+            ViewBag.Message = "Your contact page.";
+            Assembly asm = Assembly.GetAssembly(typeof(MvcApplication));
+
+            var controlleractionlist = asm.GetTypes()
+                    .Where(type => typeof(System.Web.Mvc.Controller).IsAssignableFrom(type))
+                    .SelectMany(type => type.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public))
+                    .Where(m => !m.GetCustomAttributes(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), true).Any())
+                    .Select(x => new Route { Controller = x.DeclaringType.Name.Replace("Controller",""), Action = x.Name, ReturnType = x.ReturnType.Name, Attributes = String.Join(",", x.GetCustomAttributes().Select(a => a.GetType().Name.Replace("Attribute", ""))) })
+                    .OrderBy(x => x.Controller).OrderByDescending(x=>x.Controller =="Home").ThenBy(x => x.Action).ToList();
+           
+            return View(controlleractionlist);
         }
     }
 }
--- AngularApp/Scripts/app.js
+++ AngularApp/Scripts/app.js
@@ -12,7 +12,20 @@
             {id:'1158AC91-9600-428F-BC0D-EE6B15EA30DE',name:'Filip',number:'0766554464'},
             { id: '2158AC91-9600-428F-BC0D-EE6B15EA30DE', name: 'Jan', number: '0766554465' },
             { id: '3158AC91-9600-428F-BC0D-EE6B15EA30DE', name: 'Jessica', number: '0766554466' }
-        ];
+        ]
+        vm.filip='majdzik';
+        vm.options1=[
+            {nid:1,id:'1158AC11-9600-428F-BC0D-EE6B15EA30DE',text:'option1'},
+            {nid:2,id:'1158AC21-9600-428F-BC0D-EE6B15EA30DE',text:'option2'},
+            {nid:3,id:'1158AC31-9600-428F-BC0D-EE6B15EA30DE',text:'option3'},
+            {nid:4,id:'1158AC41-9600-428F-BC0D-EE6B15EA30DE',text:'option4'},
+            {nid:5,id:'1158AC51-9600-428F-BC0D-EE6B15EA30DE',text:'option5'},
+            {nid:6,id:'1158AC61-9600-428F-BC0D-EE6B15EA30DE',text:'option6'},
+            {nid:7,id:'1158AC71-9600-428F-BC0D-EE6B15EA30DE',text:'option7'},
+            {nid:8,id:'1158AC81-9600-428F-BC0D-EE6B15EA30DE',text:'option8'},
+            {nid:9,id:'1158AC91-9600-428F-BC0D-EE6B15EA30DE',text:'option9'},
+            {nid:0,id:'1158AC01-9600-428F-BC0D-EE6B15EA30DE',text:'option0'}
+        ]
         //$scope.currentItem={}
         //#region Internal Methods        
         //#endregion
